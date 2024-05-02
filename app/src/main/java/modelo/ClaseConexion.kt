d9c3083d3cd776561959fa0526ae557bfd3de183
Fernando <jfer.orantes@gmail.com>
2024-05-02 09:39:25 -0600

    terminada la conexion

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -1,4 +1,26 @@
 package modelo
+import java.lang.Exception
+import java.sql.Connection
+import java.sql.DriverManager
 
 class ClaseConexion {
+
+    fun cadenaConexion(): Connection?{
+
+        try {
+            val url = "jdbc:oracle:thin:@10.10.0.144:1521:xe"
+
+            val user = "system"
+
+            val contra = "desarrollo"
+
+
+            val connection = DriverManager.getConnection(url, user, contra)
+                return connection
+        }
+        catch (e: Exception){
+            println("Este es el error: $e")
+            return null
+        }
+    }
 }
